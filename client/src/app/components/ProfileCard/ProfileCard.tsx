6316555ab6ce7d4565b4048427c9cf9518ad3b36
Mudasir Bashir <for.all6474@gmail.com>
2025-05-23 22:44:10 +0500

    folder structure designed

---
+++ client/src/app/components/ProfileCard/ProfileCard.tsx
@@ -0,0 +1,10 @@
+import styles from './ProfileCard.module.css'
+const ProfileCard = () => {
+    return (
+        <div className={styles.mainContainer}>
+            <div className={styles.content}>Profile card component</div>
+        </div>
+    )
+}
+
+export default ProfileCard
